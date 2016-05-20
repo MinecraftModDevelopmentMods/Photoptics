9accc836ded6730765777aaa6238ad10b5681791
Abastro <abab9579@gmail.com>
2016-05-21 07:15:41 +0900

    Fixed model not found errors; v0.3.1.1b

--- src/main/java/abr/mod/photoptics/Photoptics.java
+++ src/main/java/abr/mod/photoptics/Photoptics.java
@@ -7,6 +7,7 @@ import org.apache.logging.log4j.Logger;
 import abr.mod.photoptics.item.PhotopticsItems;
 import abr.mod.photoptics.network.PhotopticsNetworkHandler;
 import abr.mod.photoptics.processing.PhotopticsRecipes;
+import net.minecraftforge.common.MinecraftForge;
 import net.minecraftforge.fml.common.FMLCommonHandler;
 import net.minecraftforge.fml.common.Mod;
 import net.minecraftforge.fml.common.Mod.EventHandler;
@@ -45,7 +46,7 @@ public class Photoptics {
     	logger = event.getModLog();
     	
     	StellarAPIReference.getEventBus().register(new StellarAPIEventHandler());
-    	FMLCommonHandler.instance().bus().register(new TickEventHandler());
+    	MinecraftForge.EVENT_BUS.register(new TickEventHandler());
     	networkHandler.register();
     	
     	PhotopticsItems.init();
