d8233115ecc803c003ba222cc97e8d418a76d9d6
Abastro <abab9579@gmail.com>
2016-06-05 03:48:08 +0900

    Updated version.

--- src/main/java/abr/mod/photoptics/item/PhotopticsItems.java
+++ src/main/java/abr/mod/photoptics/item/PhotopticsItems.java
@@ -1,7 +1,9 @@
 package abr.mod.photoptics.item;
 
+import abr.mod.photoptics.Photoptics;
 import net.minecraft.creativetab.CreativeTabs;
 import net.minecraft.item.Item;
+import net.minecraft.util.ResourceLocation;
 import net.minecraftforge.fml.common.registry.GameRegistry;
 
 public class PhotopticsItems {
@@ -17,9 +19,10 @@ public class PhotopticsItems {
     	basicHandheldTelescope 
     			.setUnlocalizedName("photoptics.basichandheldtelescope")
     			.setCreativeTab(CreativeTabs.TOOLS).setMaxStackSize(1);
-    	
-    	GameRegistry.registerItem(basicBinoculars, "photopticsbasicbinoculars");
-    	GameRegistry.registerItem(basicHandheldTelescope, "basichandheldtelescope");
-	}
 
+    	GameRegistry.register(basicBinoculars,
+    			new ResourceLocation(Photoptics.resourceid, "photopticsbasicbinoculars"));
+    	GameRegistry.register(basicHandheldTelescope,
+    			new ResourceLocation(Photoptics.resourceid, "basichandheldtelescope"));
+	}
 }
